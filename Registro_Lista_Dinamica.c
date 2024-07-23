ce0bd5380a329a9a70c4a1b714e845bb094fa659
Millena Almeida <100153774+Millenac@users.noreply.github.com>
2024-07-23 17:12:42 -0300

    Update Registro_Lista_Dinamica.c

--- Registro_Lista_Dinamica.c
+++ Registro_Lista_Dinamica.c
@@ -5,19 +5,6 @@ int matricula;
 char nome[30];
 float salario;
 } Funcionario;
-
-Para isso, implemente uma lista dinâmica tipo Funcionario de tamanho MAX e crie funções para o seguinte:
-
-§ Inicializar a lista: um registro será considerado vazio caso a matrícula seja igual a zero;
-§ Tamanho da lista: retorna um inteiro com a quantidade de posições não vazias a lista;
-§ Inserir registro:um novo registro deve ser armazenado em uma posição vazia da lista, caso disponível;
-§ Alterar registro: realiza a alteração dos dados de um registro de uma posição da lista informada, caso exista;
-§ Apagar registro: modifica um registro de uma posição da lista informada para que se torne vazio, caso exista;
-§ Listar registros: exibe em tela uma listagem dos registros armazenados na lista.
-
-
--------------------------------------------------------------------------------------------------------------
-  
   
 #include <stdio.h>
 #include <stdlib.h>
